a7760d544d15ff016238f9302224b3ca47b799f7
justyna38 <warzecha.justyna93@gmail.com>
2023-11-19 19:31:14 +0100

    Initial Commit

---
+++ Apple-PlatformUI/Apple_PlatformUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_PlatformUIApp.swift
+//  Apple-PlatformUI
+//
+//  Created by Justyna Warzecha on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_PlatformUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
